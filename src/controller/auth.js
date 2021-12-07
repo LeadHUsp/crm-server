e6b17e9dde9a0c7810ab70d35e640adfaf1b1b4f
LeadHUsp <bodvinukr@gmail.com>
2021-12-07 19:59:19 +0300

    refactoring auth controller

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,10 +1,21 @@
 const User = require('../models/user');
+const userService = require('../service/user-service');
 const jwt = require('jsonwebtoken');
 
 class UserController {
     async registration(req, res, next) {
         try {
-        } catch (error) {}
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return next(ApiError.BadRequest('Ошибка при валидации', errors.array()));
+            }
+            const { email, password } = req.body;
+            const userData = await userService.registration(email, password);
+            res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
+            return res.json(userData);
+        } catch (e) {
+            console.log(e);
+        }
     }
     async login(req, res, next) {
         try {
--- src/services/mail-service.js
+++ src/services/mail-service.js
@@ -1,5 +1,46 @@
+import { CourierClient } from '@trycourier/courier';
+
+const courier = CourierClient({ authorizationToken: 'pk_prod_NQ5D8PFQMT47WVJTMKJG1ZBZS05F' });
+
 class MailServices {
-    async sendActivationMail(email, link) {}
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: false,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD,
+            },
+        });
+        // this.courier = CourierClient({ authorizationToken: 'pk_prod_NQ5D8PFQMT47WVJTMKJG1ZBZS05F' });
+    }
+    async sendActivationMail(email, link) {
+        await this.transporter.sendMail({
+            from: process.env.SMTP_USER,
+            to,
+            subject: 'Активация аккаунта на ' + process.env.API,
+            text: '',
+            html: `
+                    <div>
+                        <h1>Для активации перейдите по ссылке</h1>
+                        <a href="${link}">${link}</a>
+                    </div>
+                `,
+        });
+    }
+    //     const res = await courier.send({
+    //         eventId: 'courier-quickstart',
+    //         recipientId: 'bodvinukr@gmail.com',
+    //         data: {
+    //             favoriteAdjective: 'awesomeness',
+    //         },
+    //         profile: {
+    //             email: 'bodvinukr@gmail.com',
+    //         },
+    //     });
+    //     console.log(res);
+    // }
 }
 
 module.exports = new MailServices();
